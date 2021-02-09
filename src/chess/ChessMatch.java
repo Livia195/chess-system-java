64cc906041c085b6a65fcdd8650f9ad4a2908afc
Livia195 <livia.vieirarj@gmail.com>
2021-02-09 17:38:27 -0300

    colocando as peças no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,14 +1,18 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rock;
 
 public class ChessMatch {
 	private Board board;
 	
 	public ChessMatch() {
 		board = new Board (8 , 8); 
+		initialSetup();
 	}
-
+ 
 	public ChessPiece [][] getpieces(){
 		ChessPiece[][] mat =  new ChessPiece [board.getRows()][board.getColumns()];
 		for (int i=0; i<board.getRows(); i++) {
@@ -17,6 +21,12 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	
+	private void initialSetup() {
+		board.placePiece(new Rock(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
 	}
 
 }
